ae2ae8bde6b5c7e01099bdee2bfb1a0415f05517
David Lee <dlee@livefyre.com>
2014-08-05 17:05:58 -0700

    Renaming PersonalizedStreamsClient

--- test/api/personalized_stream.js
+++ test/api/personalized_stream.js
@@ -1,7 +1,7 @@
 var livefyre = require('../../lib/livefyre.js'),
 	jwt = require('jwt-simple');
 
-var PSClient = require('../../lib/api/personalized_streams.js'),
+var PersonalizedStream = require('../../lib/api/personalized_stream.js'),
 	CursorFactory = require('../../lib/factory/cursor_factory.js'),
 	Topic = require('../../lib/entity/topic.js'),
 	Constants = require('../constants.js');
@@ -18,18 +18,18 @@ exports.unit = {
 
 		var one = function(result) {
 			test.equal(result.length, 1);
-			PSClient.getTopic(two, network, '1'); 
+			PersonalizedStream.getTopic(two, network, '1'); 
 		};
 		var two = function(result) {
 			test.equal(result.label, 'ONE');
-			PSClient.deleteTopic(three, network, result); 
+			PersonalizedStream.deleteTopic(three, network, result); 
 		};
 		var three = function(result) {
 			test.equal(result, 1);
 			test.done();
 		};
 
-		PSClient.createOrUpdateTopic(one, network, '1', 'ONE');
+		PersonalizedStream.createOrUpdateTopic(one, network, '1', 'ONE');
 	},
 
 	'should test network topics api calls': function(test) {
@@ -37,50 +37,50 @@ exports.unit = {
 
 		var one = function(result) {
 			test.equal(result.length, 2);
-			PSClient.getTopics(two, network);
+			PersonalizedStream.getTopics(two, network);
 		};
 		var two = function(result) {
 			test.equal(result.length, 2);
-			PSClient.deleteTopics(three, network, result);
+			PersonalizedStream.deleteTopics(three, network, result);
 		};
 		var three = function(result) {
 			test.equal(result, 2);
 			test.done();
 		};
 
-		PSClient.createOrUpdateTopics(one, network, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
+		PersonalizedStream.createOrUpdateTopics(one, network, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
 	},
 
 	'should test network subscription api calls': function(test) {
 		test.expect(5);
 		var userToken = network.buildUserAuthToken(Constants.USER_ID, Constants.USER_ID + '@' + Constants.NETWORK_NAME, network.DEFAULT_EXPIRES);
 
-		var topics = PSClient.createOrUpdateTopics(function(result) {}, network, [{ key: 2, value: 'TWO'}, { key: 3, value: 'THREE' }]);
+		var topics = PersonalizedStream.createOrUpdateTopics(function(result) {}, network, [{ key: 2, value: 'TWO'}, { key: 3, value: 'THREE' }]);
 		var one = function(result) {
 			test.equal(result, 2);
-			PSClient.getSubscriptions(two, network, Constants.USER_ID);
+			PersonalizedStream.getSubscriptions(two, network, Constants.USER_ID);
 		};
 		var two = function(result) {
 			test.equal(result.length, 2);
-			PSClient.replaceSubscriptions(three, network, userToken, [topics[0]]);
+			PersonalizedStream.replaceSubscriptions(three, network, userToken, [topics[0]]);
 		};
 		var three = function(result) {
 			test.equal(result.removed, 1);
-			PSClient.getSubscribers(four, network, topics[0]);
+			PersonalizedStream.getSubscribers(four, network, topics[0]);
 		};
 		var four = function(result) {
 			test.equal(result.length, 1);
-			PSClient.removeSubscriptions(five, network, userToken, [topics[0]]);
+			PersonalizedStream.removeSubscriptions(five, network, userToken, [topics[0]]);
 		};
 		var five = function(result) {
 			test.equal(result, 1);
-			PSClient.deleteTopics(six, network, topics);
+			PersonalizedStream.deleteTopics(six, network, topics);
 		};
 		var six = function(result) {
 			test.done();
 		};
 
-		PSClient.addSubscriptions(one, network, userToken, topics);
+		PersonalizedStream.addSubscriptions(one, network, userToken, topics);
 	},
 
 	'tests cursors': function(test) {
@@ -102,18 +102,18 @@ exports.unit = {
 
 		var one = function(result) {
 			test.equal(result.length, 1);
-			PSClient.getTopic(two, site, '1');
+			PersonalizedStream.getTopic(two, site, '1');
 		};
 		var two = function(result) {
 			test.equal(result.label, 'ONE');
-			PSClient.deleteTopic(three, site, result);
+			PersonalizedStream.deleteTopic(three, site, result);
 		};
 		var three = function(result) {
 			test.equal(result, 1);
 			test.done();
 		};
 
-		PSClient.createOrUpdateTopic(one, site, '1', 'ONE');
+		PersonalizedStream.createOrUpdateTopic(one, site, '1', 'ONE');
 	},
 
 	'should test site topics api calls': function(test) {
@@ -121,44 +121,44 @@ exports.unit = {
 
 		var one = function(result) {
 			test.equal(result.length, 2);
-			PSClient.getTopics(two, site);
+			PersonalizedStream.getTopics(two, site);
 		};
 		var two = function(result) {
 			test.equal(result.length, 2);
-			PSClient.deleteTopics(three, site, result);
+			PersonalizedStream.deleteTopics(three, site, result);
 		};
 		var three = function(result) {
 			test.equal(result, 2);
 			test.done();
 		};
 
-		PSClient.createOrUpdateTopics(one, site, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
+		PersonalizedStream.createOrUpdateTopics(one, site, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
 	},
 
 	'should test site collection topic api calls': function(test) {
 		test.expect(4);
 
-		var topics = PSClient.createOrUpdateTopics(function(result) {}, site, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
+		var topics = PersonalizedStream.createOrUpdateTopics(function(result) {}, site, [{ key: 2, value: 'TWO' }, { key: 3, value: 'THREE' }]);
 		var one = function(result) {
 			test.equal(result, 2);
-			PSClient.getCollectionTopics(two, site, Constants.COLLECTION_ID);
+			PersonalizedStream.getCollectionTopics(two, site, Constants.COLLECTION_ID);
 		};
 		var two = function(result) {
 			test.equal(result.length, 2);
-			PSClient.replaceCollectionTopics(three, site, Constants.COLLECTION_ID, [topics[0]]);
+			PersonalizedStream.replaceCollectionTopics(three, site, Constants.COLLECTION_ID, [topics[0]]);
 		};
 		var three = function(result) {
 			test.equal(result.removed, 1);
-			PSClient.removeCollectionTopics(four, site, Constants.COLLECTION_ID, [topics[0]]);
+			PersonalizedStream.removeCollectionTopics(four, site, Constants.COLLECTION_ID, [topics[0]]);
 		};
 		var four = function(result) {
 			test.equal(result, 1);
-			PSClient.deleteTopics(five, site, topics);
+			PersonalizedStream.deleteTopics(five, site, topics);
 		};
 		var five = function(result) {
 			test.done();
 		};
 
-		PSClient.addCollectionTopics(one, site, Constants.COLLECTION_ID, topics);
+		PersonalizedStream.addCollectionTopics(one, site, Constants.COLLECTION_ID, topics);
 	}
 }
