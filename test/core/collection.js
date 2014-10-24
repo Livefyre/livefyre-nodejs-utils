648cb9029c41e3f3b1bedd4ad722db88851878a4
David Lee <dlee@livefyre.com>
2014-10-23 18:17:17 -0700

    updating collection test

--- test/core/collection.js
+++ test/core/collection.js
@@ -38,6 +38,9 @@ exports.unit = {
         try {
             Collection.init(site, CollectionType.RATINGS, 'title', 'articleId', 'test.com');
         } catch (err) { }
+        try {
+            Collection.init(site, 'bad type', c.TITLE, c.ARTICLE_ID, c.URL);
+        } catch (err) { }
         test.done();
     },
 
@@ -47,43 +50,24 @@ exports.unit = {
         test.done();
     },
 
-//    'should check various strings for type and ensure that they are set in the correct field': function(test) {
-//        var token = site.buildCollectionMetaToken('title', 'articleId', 'http://livefyre.com', { tags: 'tag', type: 'reviews' });
-//        var decoded = jwt.decode(token, c.SITE_KEY);
-//        test.equals(jwt.decode(token, c.SITE_KEY)['type'], 'reviews');
-//
-//        token = site.buildCollectionMetaToken('title', 'articleId', 'http://livefyre.com', { tags: 'tag', type: 'liveblog' });
-//        decoded = jwt.decode(token, c.SITE_KEY);
-//
-//        test.equals(jwt.decode(token, c.SITE_KEY)['type'], 'liveblog');
-//
-//        test.done();
-//    },
-//
     'should return a valid checksum': function(test) {
         var collection = site.buildLiveCommentsCollection('title', 'articleId', 'http://livefyre.com');
         collection.data.tags = 'tags';
         test.equals('8bcfca7fb2187b1dcb627506deceee32', collection.buildChecksum());
         test.done();
+    },
+    //TODO: change to try/catch
+    'should check different variations of valid and invalid urls': function(test) {
+        test.ok(util.isError(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'test.com')));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'http://test.com:8000'));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'https://test.com'));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'ftp://test.com/'));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, "https://test.com/path/test.-_~!$&'()*+,=:@/dash"));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'http://清华大学.cn'));
+        test.ok(site.buildLiveCommentsCollection(c.TITLE, c.ARTICLE_ID, 'http://www.mysite.com/myresumé.html'));
+
+        test.done();
     }
-//
-//    'should check if buildChecksum has a valid url and title is less than 256 char': function(test) {
-//        test.ok(util.isError(site.buildChecksum('title', 'test.com', 'tag')));
-//        test.ok(util.isError(site.buildChecksum('1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456', 'http://test.com', 'tag')));
-//        test.done();
-//    },
-//
-//    'should check different variations of valid and invalid urls': function(test) {
-//        test.ok(util.isError(site.buildChecksum('', 'test.com', '')));
-//        test.ok(site.buildChecksum('', 'http://test.com:8000', ''));
-//        test.ok(site.buildChecksum('', 'https://test.com/', ''));
-//        test.ok(site.buildChecksum('', 'ftp://test.com/', ''));
-//        test.ok(site.buildChecksum('', "https://test.com/path/test.-_~!$&'()*+,=:@/dash", ''));
-//        test.ok(site.buildChecksum('', 'http://清华大学.cn', ''));
-//        test.ok(site.buildChecksum('', 'http://www.mysite.com/myresumé.html', ''));
-//
-//        test.done();
-//    },
 //
 //    'should test basic site api calls': function(test) {
 //        test.expect(1);
